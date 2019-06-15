438c69ecbae20d7c3c9162f3b45ef12a2cc41b81
Patrik Hermansson <hermansson.patrik@gmail.com>
2019-06-15 09:25:12 +0200

    Add tags

--- setup.py
+++ setup.py
@@ -5,14 +5,15 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="vlcHttp-bphermansson",
+    packages = ['vlcHttp-bphermansson'],
     version="0.0.1",
     author="Patrik Hermansson",
     author_email="hermansson.patrik@gmail.com",
     description="A package to control VLC via Http",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
-    packages=setuptools.find_packages(),
+    url="https://github.com/bphermansson/vlcHttp",
+    #packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
